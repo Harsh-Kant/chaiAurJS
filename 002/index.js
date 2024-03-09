e43bd3d7fb667f5991a8f9f58fb6d7fa51ad9bc1
Harsh-Kant <62377286+Harsh-Kant@users.noreply.github.com>
2024-03-09 09:54:39 +0000

    nitin data

---
+++ 002/index.js
@@ -0,0 +1,12 @@
+const fdfsg = "gcfj";
+let mvh = "liuoi";
+var jft = "nvnv";
+aaqw = "werr"; 
+console.table([ dfsg, mvh, jft, aaqw]);
+
+// fdfsg = "jghk"; const can't be changed
+// don't use var due to it's nature in block scope and functional scope
+mvh = "hhnnnj";
+jft = "cxdsefs";
+aaqw = "xvvn";
+console.table([fdfsg, mvh, jft, aaqw]);
