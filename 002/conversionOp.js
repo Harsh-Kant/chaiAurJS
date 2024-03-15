c70416e50f4da681b64176fef9eb52c97db299a2
Harsh-Kant <62377286+Harsh-Kant@users.noreply.github.com>
2024-03-15 17:05:11 +0000

    type conversion

---
+++ 002/conversionOp.js
@@ -0,0 +1,15 @@
+let score = "Harsh"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valInNum = Number(score)
+
+console.log(typeof valInNum)
+console.log(valInNum)
+
+// "33" => 33
+// "33abc" => NaN
+//true = 1; false = 0
+// "" => false
+// "Harsh" => true
